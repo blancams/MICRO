b9b629f686bb23e3ca896bc2c84594cdd951cd24
SLiezzan <sliezan19@gmail.com>
2018-04-13 20:59:28 +0200

    Entrega lista

--- P3/pract3a.asm
+++ P3/pract3a.asm
@@ -15,43 +15,43 @@ ASSUME CS: PRAC3A
 
 ; Coding of checkSecretNumber, specifying FAR to use LARGE model
 _checkSecretNumber PROC FAR
-PUSH BP
-MOV BP, SP
-PUSH BX DX ES 			; Saves registers
+		PUSH BP
+		MOV BP, SP
+		PUSH BX DX ES 			; Saves registers
 
-LES BX, [BP + 6] 		; Reads parameters from stack (array of 4 char* elements, i.e. 4 bytes, 1 digit per byte)
-MOV AL, ES:[BX]			; Saves first digit in AL
-MOV AH, ES:[BX+1]		; Saves second digit in AH
-MOV DL, ES:[BX+2]		; Saves third digit in DL
-MOV DH, ES:[BX+3]		; Saves fourth digit in DH
-CMP AL, AH				; Compares first and second digit. If they are equal, jumps to bye
+		LES BX, [BP + 6] 		; Reads parameters from stack (array of 4 char* elements, i.e. 4 bytes, 1 digit per byte)
+		MOV AL, ES:[BX]			; Saves first digit in AL
+		MOV AH, ES:[BX+1]		; Saves second digit in AH
+		MOV DL, ES:[BX+2]		; Saves third digit in DL
+		MOV DH, ES:[BX+3]		; Saves fourth digit in DH
+		CMP AL, AH				; Compares first and second digit. If they are equal, jumps to bye
+		JE bye
+		CMP AL, DL				; Compares first and third digit. If they are equal, jumps to bye
+		JE bye
+		CMP AL, DH				; Compares first and fourth digit. If they are equal, jumps to bye
+		JE bye
+		CMP AH, DL				; Compares second and third digit. If they are equal, jumps to bye
+		JE bye
+		CMP AH, DH				; Compares second and fourth digit. If they are equal, jumps to bye
 JE bye
-CMP AL, DL				; Compares first and third digit. If they are equal, jumps to bye
-JE bye
-CMP AL, DH				; Compares first and fourth digit. If they are equal, jumps to bye
-JE bye
-CMP AH, DL				; Compares second and third digit. If they are equal, jumps to bye
-JE bye
-CMP AH, DH				; Compares second and fourth digit. If they are equal, jumps to bye
-JE bye
-CMP DH, DL				; Compares third and fourth digit. If they are equal, jumps to bye
-JE bye
+		CMP DH, DL				; Compares third and fourth digit. If they are equal, jumps to bye
+		JE bye
 
-MOV AX, 0				; If this instruction is executed, there is no coincidence between digits and 0 is returned
-JMP nobye				; Jumps to end of program
-bye:   MOV AX, 1 		; If this instruction is executed, there has been a coincidence between digits and 1 is returned
-nobye: POP ES DX BX BP 	; Recovers used registers
+		MOV AX, 0				; If this instruction is executed, there is no coincidence between digits and 0 is returned
+		JMP nobye				; Jumps to end of program
+bye:   MOV AX, 1 				; If this instruction is executed, there has been a coincidence between digits and 1 is returned
+nobye: POP ES DX BX BP 			; Recovers used registers
 RET
 _checkSecretNumber ENDP
 
 ; Coding of fillUpAttempt, specifying FAR to use LARGE model
 _fillUpAttempt PROC FAR
-PUSH BP
-MOV BP, SP
-PUSH DI CX BX DX ES 			; Saves registers
+		PUSH BP
+		MOV BP, SP
+		PUSH DI CX BX DX ES 	; Saves registers
 
-MOV AX, [BP + 6] 				; Reads first parameter, the 4-digit number
-LES BX, [BP + 8] 				; Reads second parameter, the array of 4 char variables to be filled
+		MOV AX, [BP + 6] 		; Reads first parameter, the 4-digit number
+		LES BX, [BP + 8] 		; Reads second parameter, the array of 4 char variables to be filled
 
 ; This is the method with which we coded the first exercise of the previous practice
 ; It splits a 4-digit number into its four digits, storing each digit in a byte
@@ -65,7 +65,7 @@ bucle:	DEC DI					; Decrementation of DI to keep looping
 		CMP DI, 0h				; Loop stops if the result of the division was 0
 		JNE bucle
 
-POP ES DX BX CX DI BP			; Recovers used registers
+		POP ES DX BX CX DI BP	; Recovers used registers
 RET
 _fillUpAttempt ENDP
 PRAC3A ENDS ; End of Segment
--- P3/pract3b.asm
+++ P3/pract3b.asm
@@ -15,61 +15,61 @@ ASSUME CS: PRAC3B
 
 ; Coding of computeMatches, specifying FAR to use LARGE model
 _computeMatches PROC FAR
-PUSH BP
-MOV BP, SP
-PUSH BX DX DS CX SI ES ; Save registers
+		PUSH BP
+		MOV BP, SP
+		PUSH BX DX DS CX SI ES 			; Save registers
 
-LDS CX, [BP + 6]   ; We save the offset of secretNum in CX, and the segment in DS
-LES BX, [BP + 10]  ; The offset of the array attemptDigits is saved in BX, and the segment in ES
-MOV BP, CX		   ; The offset of secretNum is now in BP
+		LDS CX, [BP + 6]   				; We save the offset of secretNum in CX, and the segment in DS
+		LES BX, [BP + 10]  				; The offset of the array attemptDigits is saved in BX, and the segment in ES
+		MOV BP, CX		  				; The offset of secretNum is now in BP
 
-MOV AX, 0          ; Initialization of AX, that will represent the number of matches
-MOV SI, 0		   ; Initialization of SI
-it: 	MOV DX, DS:[BP][SI]   ; We save in DX secretNum[SI]
-		MOV CX, ES:[BX][SI]	  ; We save in CX attemptDigits[SI]
-		CMP DL, CL			  ; As we are working with char variables, we only need to compare one byte
-		JNE cnt				  ; if they aren't equal, we jump to avoid incrementing the counter DI
-		INC AX				  ; if they are equal, we increment AX in one
-cnt:	INC SI				  ; We continue incrementing SI
-		CMP SI, 4			  ; If SI is equal to 4, that means the arrays have been computed
-		JE bye				  ; and we jump to the final block of the code
-		JMP it				  ; if SI is not equal to 4, we continue to compare the elements of the arrays
-							  ; Program ends when SI = 4
+		MOV AX, 0          				; Initialization of AX, that will represent the number of matches
+		MOV SI, 0		   				; Initialization of SI
+it: 	MOV DX, DS:[BP][SI]				; We save in DX secretNum[SI]
+		MOV CX, ES:[BX][SI]				; We save in CX attemptDigits[SI]
+		CMP DL, CL						; As we are working with char variables, we only need to compare one byte
+		JNE cnt							; if they aren't equal, we jump to avoid incrementing the counter DI
+		INC AX							; if they are equal, we increment AX in one
+cnt:	INC SI							; We continue incrementing SI
+		CMP SI, 4						; If SI is equal to 4, that means the arrays have been computed
+		JE bye							; and we jump to the final block of the code
+		JMP it							; if SI is not equal to 4, we continue to compare the elements of the arrays
+										; Program ends when SI = 4
 bye:	POP ES SI CX DS DX BX BP		; Recovers registers
-		RET
+RET
 _computeMatches ENDP
 
 
 ; Coding of computeSemiMatches, specifying FAR to use LARGE model
 _computeSemiMatches PROC FAR
-PUSH BP
-MOV BP, SP
-PUSH BX DX DS CX SI DI ES; Save registers
+		PUSH BP
+		MOV BP, SP
+		PUSH BX DX DS CX SI DI ES		; Save registers
 
-LDS CX, [BP + 6] 	; Save in CX the offset of secretNum, and in DS the segment 
-LES BX, [BP + 10]   ; Save in BX the offset of attemptDigits, and in ES the segment 
-MOV BP, CX			; Now the offset of secretNum is in BP
+		LDS CX, [BP + 6] 				; Save in CX the offset of secretNum, and in DS the segment 
+		LES BX, [BP + 10]   			; Save in BX the offset of attemptDigits, and in ES the segment 
+		MOV BP, CX						; Now the offset of secretNum is in BP
 
-MOV AX, 0    ; Initialization of AX
-MOV SI, 4    ; Initialization of SI
-att:	MOV DI, 0             ; Initialization of DI
-		CMP SI, 0             ; If SI is equal to 0 we jump to the final block of code	
-		JE b				  ; If SI is not equal to 0, we keep computing
-		DEC SI				  ; Decrementation of SI
-h:		MOV CX, DS:[BP][SI]   ; Save in CX secretNum[SI]
-		MOV DX, ES:[BX][DI]	  ; Save in DX attemptDigits[DI]
-		CMP CL, DL			  ; As we are working with chars, we only compare the last 8 bits
-		JNE cn				  ; If they are not equal, we jump and keep computing
-		CMP SI, DI			  ; If they are equal, we compare the indexes, that is, SI and DI
-		JE cn				  ; If SI and DI are equal, that means it is a match, not a semi-match, so we keep computing
-		INC AX				  ; If they are not equal, we increment the number of semi-matches (AX) in one
-cn:		INC DI				  ; We increment DI in one to keep going through attemptDigits
-		CMP DI, 4			  ; We compare DI with 4, 
-		JNE h				  ; and if they are not equal, we keep comparing with secretNum[SI]
-		JMP att               ; If they are equal, we jump and advance to compare attemptDigits with secretNum[SI-1]
-							  ; Program ends when SI = 0
-b:		POP ES DI SI CX DS DX BX BP  ; Recovers registers
-		RET
+		MOV AX, 0    					; Initialization of AX
+		MOV SI, 4    					; Initialization of SI
+att:	MOV DI, 0             			; Initialization of DI
+		CMP SI, 0             			; If SI is equal to 0 we jump to the final block of code	
+		JE b				  			; If SI is not equal to 0, we keep computing
+		DEC SI				 			; Decrement of SI
+h:		MOV CX, DS:[BP][SI]   			; Save in CX secretNum[SI]
+		MOV DX, ES:[BX][DI]	  			; Save in DX attemptDigits[DI]
+		CMP CL, DL			  			; As we are working with chars, we only compare the last 8 bits
+		JNE cn				  			; If they are not equal, we jump and keep computing
+		CMP SI, DI			  			; If they are equal, we compare the indexes, that is, SI and DI
+		JE cn				  			; If SI and DI are equal, that means it is a match, not a semi-match, so we keep computing
+		INC AX				  			; If they are not equal, we increment the number of semi-matches (AX) in one
+cn:		INC DI				  			; We increment DI in one to keep going through attemptDigits
+		CMP DI, 4			  			; We compare DI with 4, 
+		JNE h				  			; and if they are not equal, we keep comparing with secretNum[SI]
+		JMP att               			; If they are equal, we jump and advance to compare attemptDigits with secretNum[SI-1]
+										; Program ends when SI = 0
+b:		POP ES DI SI CX DS DX BX BP  	; Recovers registers
+RET
 _computeSemiMatches ENDP
 PRAC3B ENDS ; End of Segment
 END
